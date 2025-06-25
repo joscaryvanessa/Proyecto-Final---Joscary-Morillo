7fe5a7078da49ee7d192d99afe426ef8445f8e90
joscary <joscary__vanessa@hotmail.com>
2025-06-25 13:56:07 -0300

    feat: implementa menú hamburguesa responsive para móvil en navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,23 +21,7 @@ export default function AppNavbar() {
   return (
     <Navbar expand="lg" variant="dark">
       <Container>
-        <Nav className="left-nav">
-          <Nav.Link as={NavLink} to="/" className="text-white">
-            Inicio
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/productos" className="text-white">
-            Productos
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/contacto" className="text-white">
-            <BsEnvelope style={{ marginRight: 4, marginBottom: 2 }} />
-            Contacto
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/faq" className="text-white">
-            Preguntas Frecuentes
-          </Nav.Link>
-        </Nav>
-
-        <Navbar.Brand className="mx-auto">
+        <Navbar.Brand className="navbar-brand-mobile">
           <img
             src={logo}
             alt="Logo"
@@ -45,36 +29,65 @@ export default function AppNavbar() {
             style={{ width: '50px', height: '50px' }}
           />
         </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         
-        <Nav className="right-nav">
-          {!isLoggedIn ? (
-            <Nav.Link as={NavLink} to="/login" className="text-white">
-              <BsBoxArrowInRight style={{ marginRight: 4, marginBottom: 2 }} />
-              Iniciar Sesion
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="left-nav">
+            <Nav.Link as={NavLink} to="/" className="text-white">
+              Inicio
             </Nav.Link>
-          ) : (
-            <Nav.Link onClick={handleLogout} className="text-white">
-              <BsBoxArrowRight style={{ marginRight: 4, marginBottom: 2 }} />
-              Cerrar Sesión
+            <Nav.Link as={NavLink} to="/productos" className="text-white">
+              Productos
             </Nav.Link>
-          )}
-          <Nav.Link
-            as={NavLink}
-            to="/carrito"
-            className="text-white position-relative"
-          >
-            <i className="bi bi-cart-fill"></i>
-            {cart.length > 0 && (
-              <Badge
-                bg="danger"
-                pill
-                className="position-absolute top-0 start-100 translate-middle"
-              >
-                {cart.length}
-              </Badge>
+            <Nav.Link as={NavLink} to="/contacto" className="text-white">
+              <BsEnvelope style={{ marginRight: 4, marginBottom: 2 }} />
+              Contacto
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/faq" className="text-white">
+              Preguntas Frecuentes
+            </Nav.Link>
+          </Nav>
+
+          <Navbar.Brand className="navbar-brand-desktop">
+            <img
+              src={logo}
+              alt="Logo"
+              className="navbar-logo rounded-circle"
+              style={{ width: '50px', height: '50px' }}
+            />
+          </Navbar.Brand>
+          
+          <Nav className="right-nav">
+            {!isLoggedIn ? (
+              <Nav.Link as={NavLink} to="/login" className="text-white">
+                <BsBoxArrowInRight style={{ marginRight: 4, marginBottom: 2 }} />
+                Iniciar Sesion
+              </Nav.Link>
+            ) : (
+              <Nav.Link onClick={handleLogout} className="text-white">
+                <BsBoxArrowRight style={{ marginRight: 4, marginBottom: 2 }} />
+                Cerrar Sesión
+              </Nav.Link>
             )}
-          </Nav.Link>
-        </Nav>
+            <Nav.Link
+              as={NavLink}
+              to="/carrito"
+              className="text-white position-relative"
+            >
+              <i className="bi bi-cart-fill"></i>
+              {cart.length > 0 && (
+                <Badge
+                  bg="danger"
+                  pill
+                  className="position-absolute top-0 start-100 translate-middle"
+                >
+                  {cart.length}
+                </Badge>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
